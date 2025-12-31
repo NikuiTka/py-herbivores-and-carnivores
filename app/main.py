608cc29d3df2e7941a83d7c8bda25713b74a23c5
Nikita <151358720+NikuiTka@users.noreply.github.com>
2025-12-31 10:10:28 +0200

    Add Animal hierarchy with Herbivore and Carnivore classes
    Implemented Animal, Herbivore, and Carnivore classes with basic functionality.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,38 @@
 # write your code here
+from typing import Union
+
+
+class Animal:
+    alive: list["Animal"] = []
+
+    def __init__(
+            self,
+            name: str
+    ) -> None:
+        self.name = name
+        self.health = 100
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(
+            self,
+            target: Union[Herbivore, "Carnivore"]
+    ) -> None:
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
+            if target.health <= 0:
+                Animal.alive.remove(target)
